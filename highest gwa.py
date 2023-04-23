c4c9844c262ce5c7de864e4ee3317b745a33ca48
aieckxis <aleckxiskate@gmail.com>
2023-04-23 13:15:45 +0800

    loop

--- highest gwa.py
+++ highest gwa.py
@@ -3,12 +3,15 @@
 
 # Open the input file.
 with open('students.txt', 'r') as f:
-# Set up variables to determine which student has the highest GWA.
+    # Set up variables to determine which student has the highest GWA.
     highest_gwa = 1
     student_name = ''
 # Read the file line by line.
-# Separate the student name from the GWA in the line.
-# Convert the GWA into a float.
+for line in f:
+    # Separate the student name from the GWA in the line.
+    name, gwa = line.strip().split(',')
+    # Convert the GWA into a float.
+    gwa = float(gwa)
 # Check if the student has a higher GWA than the current highest GWA
 # Update the highest GWA and the student's name with the highest GWA.
 # Print the name and GWA of the pupil with the highest GWA.
