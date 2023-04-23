38eb633b7963d6e634f4ab2ab04c38b37d75b91c
aieckxis <aleckxiskate@gmail.com>
2023-04-23 13:34:22 +0800

    error

--- highest gwa.py
+++ highest gwa.py
@@ -6,12 +6,16 @@ with open('students.txt', 'r') as f:
     # Set up variables to determine which student has the highest GWA.
     highest_gwa = 1
     student_name = ''
-# Read the file line by line.
-for line in f:
-    # Separate the student name from the GWA in the line.
-    name, gwa = line.strip().split(',')
-    # Convert the GWA into a float.
-    gwa = float(gwa)
-# Check if the student has a higher GWA than the current highest GWA
-# Update the highest GWA and the student's name with the highest GWA.
+    # Read the file line by line.
+    for line in f:
+        # Separate the student name from the GWA in the line.
+        name, gwa = line.strip().split(',')
+        # Convert the GWA into a float.
+        gwa = float(gwa)
+        # Check if the student has a higher GWA than the current highest GWA
+        if gwa < highest_gwa:
+            # Update the highest GWA and the student's name with the highest GWA.
+            highest_gwa = gwa
+            student_name = name
 # Print the name and GWA of the pupil with the highest GWA.
+print(f"The student with the highest GWA is", student_name, "with the GWA of", highest_gwa, ".")
