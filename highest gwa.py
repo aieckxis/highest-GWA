6c86822944f2a9d09b1e9b84d31150b1a302ca29
aieckxis <aleckxiskate@gmail.com>
2023-04-23 11:39:11 +0800

    pseudocode

---
+++ highest gwa.py
@@ -0,0 +1,11 @@
+# Bernabe, Aleckxis Kate V.
+# BSCpE 1-4
+
+# Open the input file.
+# Set up variables to determine which student has the highest GWA.
+# Read the file line by line.
+# Separate the student name from the GWA in the line.
+# Convert the GWA into a float.
+# Check if the student has a higher GWA than the current highest GWA
+# Update the highest GWA and the student's name with the highest GWA.
+# Print the name and GWA of the pupil with the highest GWA.
